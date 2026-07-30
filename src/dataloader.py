5dd7e6b06445f3a0f698fc82eb273f97b62ce0cf
bely-lab <tslatebely@gmail.com>
2026-07-30 12:14:25 +0300

    data loader

---
+++ src/dataloader.py
@@ -0,0 +1,19 @@
+from torch.utils.data import DataLoader
+
+from dataset import BrainMRIDataset
+
+
+dataset = BrainMRIDataset("data/processed")
+
+loader = DataLoader(
+    dataset,
+    batch_size=2,
+    shuffle=True
+)
+
+for images, names in loader:
+
+    print(images.shape)
+    print(names)
+
+    break
